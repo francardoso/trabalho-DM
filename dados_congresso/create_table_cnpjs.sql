39f6c1600517874c06534330ab08621468c6b882
Maycon <maycon.antonio.gesser@gmail.com>
2018-10-17 12:07:14 -0300

    adiciona script para criação de tabela cnpjs

---
+++ dados_congresso/create_table_cnpjs.sql
@@ -0,0 +1,11 @@
+CREATE TABLE public.cnpjs
+(
+  cnpj character varying(50),
+  razao_social text,
+  nome_fantasia text,
+  uf text,
+  municipio text,
+  cep text,
+  data_abertura character varying(20),
+  situacao text
+)
